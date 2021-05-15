414dc1a0b67ac40f3700e2e175fb64621bcdf3e9
Jack Domleo <jackdomleo7@gmail.com>
2021-05-15 19:52:47 +0100

    Create sample component with interface

---
+++ src/components/ListOfLinks.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="hello">
+    <h3>My List of Links 🙌</h3>
+    <ul>
+      <li v-for="item in list" :key="item.url">
+        <a :href="item.url">
+          {{ item.text }}
+        </a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue';
+
+export interface ILink {
+  text: string;
+  url: string;
+}
+
+export default defineComponent({
+  name: 'ListOfLinks',
+  props: {
+    list: {
+      type: Array as PropType<ILink[]>,
+      required: true
+    },
+  },
+});
+</script>
+
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+a {
+  color: #42b983;
+}
+</style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,32 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <list-of-links :list="myList" />
   </div>
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import ListOfLinks, { ILink } from '@/components/ListOfLinks.vue';
+
+export default defineComponent({
+  name: 'Home',
+  components: {
+    ListOfLinks,
+  },
+  data () {
+    return {
+      myList: [
+        {
+          text: 'vue-jest repo',
+          url: 'https://github.com/vuejs/vue-jest'
+        },
+        {
+          text: 'vue-jest interface issue',
+          url: 'https://github.com/vuejs/vue-jest/issues/189'
+        }
+      ] as ILink[]
+    }
+  }
+});
+</script>
